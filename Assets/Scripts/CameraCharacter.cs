a95d4665215a6198a2b13de7b4b1f572eb2851ef
TalhaAkkoyun <talhaakkoyun03@gmail.com>
2020-09-16 20:19:26 +0300

    Initial commit

---
+++ Assets/Scripts/CameraCharacter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraCharacter : MonoBehaviour
+{
+
+    public float spawnHelper = 4.5f;
+    public GameObject ball;
+    public float ballForce = 700;
+    public float ballCount;
+    private Camera _cam;
+    public float camMove;
+    public Text ballText;
+
+
+    
+    // Use this for initialization
+    void Start()
+    {
+
+        _cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 2.5f);
+
+        float mousePosx = Input.mousePosition.x;
+        float mousePosy = Input.mousePosition.y;
+
+
+        Vector3 BallInstantiatePoint = _cam.ScreenToWorldPoint(new Vector3(mousePosx, mousePosy, _cam.nearClipPlane + spawnHelper));
+        ballText.text = " ball =" + ballCount;
+
+
+        if (Input.GetMouseButtonDown(0) && ballCount > 0)
+        {
+
+            ballCount--;
+            GameObject ballRigid;
+            ballRigid = Instantiate(ball, BallInstantiatePoint, transform.rotation) as GameObject;
+            ballRigid.GetComponent<Rigidbody>().AddForce(Vector3.forward * ballForce);
+        }
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    CameraCharacter cameraCharacter;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        cameraCharacter = Camera.main.GetComponent<CameraCharacter>();
+
+     }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("BallScore"))
+        {
+            Debug.Log("where is my fucking point");
+            Destroy(other.gameObject);
+            Debug.Log("ballCount");
+            cameraCharacter.ballCount += 3;
+        }
+    }
+}
